a5ded1d2c7204b49c0a89f5f66126f7279d4b389
xiphmont <xiphmont@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2000-08-30 06:09:22 +0000

    autoconf changes to handle looking for bit-width types on yet more platforms without barfing.  Cleaner than before, hopefully works better too.
    Monty

--- vorbis-plugins/winamp/vorbis.c
+++ vorbis-plugins/winamp/vorbis.c
@@ -79,7 +79,7 @@ size_t read_func(void *ptr, size_t size, size_t nmemb, void *datasource)
 		return 0; /* It failed */
 }
 
-int seek_func(void *datasource, int64_t offset, int whence)
+int seek_func(void *datasource, ogg_int64_t offset, int whence)
 { /* Note that we still need stdio.h even though we don't use stdio, 
    * in order to get appropriate definitions for SEEK_SET, etc.
    */
