0603b966a36686471257ddb16bd5fd18e765aa46
xiphmont <xiphmont@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2001-08-07 03:47:33 +0000

    Incremental commit

--- vorbis/lib/os.h
+++ vorbis/lib/os.h
@@ -13,7 +13,7 @@
  ********************************************************************
 
  function: #ifdef jail to whip a few platforms into the UNIX ideal.
- last mod: $Id: os.h,v 1.26 2001/06/04 05:50:10 xiphmont Exp $
+ last mod: $Id: os.h,v 1.26.2.1 2001/08/07 03:47:22 xiphmont Exp $
 
  ********************************************************************/
 
@@ -50,9 +50,6 @@
 #endif
 #ifdef HAVE_EXPF
 #  define exp expf
-#endif
-#ifdef HAVE_POWF
-#  define pow powf
 #endif
 #ifdef HAVE_ACOSF
 #  define acos acosf
