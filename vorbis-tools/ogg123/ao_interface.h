db83e44b537a60793486e8f6813f42643ddd7c7a
kcarnold <kcarnold@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2001-08-10 20:50:26 +0000

    Forgot that I added this.

---
+++ vorbis-tools/ogg123/ao_interface.h
@@ -0,0 +1,43 @@
+/********************************************************************
+ *                                                                  *
+ * THIS FILE IS PART OF THE OggVorbis SOFTWARE CODEC SOURCE CODE.   *
+ * USE, DISTRIBUTION AND REPRODUCTION OF THIS SOURCE IS GOVERNED BY *
+ * THE GNU PUBLIC LICENSE 2, WHICH IS INCLUDED WITH THIS SOURCE.    *
+ * PLEASE READ THESE TERMS BEFORE DISTRIBUTING.                     *
+ *                                                                  *
+ * THE Ogg123 SOURCE CODE IS (C) COPYRIGHT 2000-2001                *
+ * by Kenneth C. Arnold <ogg@arnoldnet.net> AND OTHER CONTRIBUTORS  *
+ * http://www.xiph.org/                                             *
+ *                                                                  *
+ ********************************************************************
+ 
+ last mod: $Id: ao_interface.h,v 1.1.2.1 2001/08/10 20:50:26 kcarnold Exp $
+ 
+********************************************************************/
+
+/* ogg123's interface to libao */
+
+#ifndef __AO_INTERFACE_H
+#define __AO_INTERFACE_H
+
+#include <ao/ao.h>
+
+/* For facilitating output to multiple devices */
+typedef struct devices_s {
+  int driver_id;
+  ao_device *device;
+  ao_option *options;
+  char *filename;
+  struct devices_s *next_device;
+} devices_t;
+
+devices_t *append_device(devices_t * devices_list, int driver_id,
+                         ao_option * options, char *filename);
+size_t devices_write(void *ptr, size_t size, size_t nmemb, devices_t * d);
+int add_option(ao_option ** op_h, const char *optstring);
+int get_default_device(void);
+void ao_onexit (int exitcode, void *devices);
+void close_audio_devices (devices_t *devices);
+void free_audio_devices (devices_t *devices);
+
+#endif /* __AO_INTERFACE_H */
