6241a768806b8a3b34b5daa98c240c8b3a212c17
jack <jack@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2001-09-03 23:27:01 +0000

    This is a working oggmerge that supports MIDI + Ogg Vorbis files.  Hooks for other media types are there (just need packetizer plugins written) and MNG should follow soon in a commit from Ralph.
    oggmerge only takes degenerate .ogg streams that contain vorbis files and
    type 0 midi files at this point.
    
    Updated build system and TODO for this new set of code.

--- ogg-tools/oggmerge/oggmerge.h
+++ ogg-tools/oggmerge/oggmerge.h
@@ -1,29 +1,63 @@
-/*
-	oggmerge -- utility for splicing together ogg bitstreams
-			from component media subtypes
-
-	oggmerge.h
-
-	Copyright 2000 Ralph Giles <Ralph_Giles@telus.net>
-
-	Distributed under the GPL
-	see the file COPYING for details
-	or visit http://www.gnu.org/copyleft/gpl.html
+/* oggmerge.h
+**
 */
 
-#ifndef OGGMERGE_H
-#define OGGMERGE_H
+#ifndef __OGGMERGE_H__
+#define __OGGMERGE_H__
 
 #include <ogg/ogg.h>
 
-/* state storage structure */
 typedef struct {
-	char	*infile, *outfile;
-	FILE	*in,*out;
-	int	quiet,verbose;
-	ogg_stream_state *os;
-	ogg_sync_state *oy;
+	ogg_page *og;
+	u_int64_t timestamp;
+} oggmerge_page_t;
+
+typedef struct {
+	void *private;
+} oggmerge_state_t;
+
+typedef struct filelist_tag {
+	char *name;
+	FILE *fp;
+
+	int type;
+
+	oggmerge_state_t state;
+	oggmerge_page_t *page;
+	int status;
+	unsigned serialno;
+
+	int (*state_init)(oggmerge_state_t *state, int serialno);
+	int (*data_in)(oggmerge_state_t *state, char *buffer, unsigned long size);
+	oggmerge_page_t *(*page_out)(oggmerge_state_t *state);
+
+	struct filelist_tag *next;
+} filelist_t;
+
+typedef struct {
+	char *outfile;
+	FILE *out;
+	filelist_t *input;
+	int quiet;
+	int verbose;
 } param_t;
 
+/* errors */
+#define EMOREDATA -1
+#define EMALLOC -2
+#define EBADHEADER -3
+#define EBADEVENT -4
+#define EOTHER -5
 
-#endif /* OGGMERGE_H */
+/* types */
+#define TYPEUNKNOWN 0
+#define TYPEVORBIS 1
+#define TYPEMIDI 2
+#define TYPEMNG 3
+
+#endif  /* __OGGMERGE_H__ */
+
+
+
+
+
