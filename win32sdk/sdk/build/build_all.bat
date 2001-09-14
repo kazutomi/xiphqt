f24a7b8ef48f59b4c520fc6c6d233ee8290a4618
cwolf <cwolf@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2001-09-14 01:53:25 +0000

    Add support for automated build for example projects

---
+++ win32sdk/sdk/build/build_all.bat
@@ -0,0 +1,48 @@
+@echo off
+rem $Id: build_all.bat,v 1.1 2001/09/14 01:53:24 cwolf Exp $
+rem
+rem Invoke as "build_all.bat CLEAN" to clean all targets
+rem
+if ."%SDKHOME%"==."" goto notset
+
+if not exist %SDKHOME%\lib\ogg.lib goto nolib
+
+rem If one of the makefiles doesn't exist,
+rem assume they all need to be generated
+rem
+if not exist %SDKHOME%\build\examples.mak (
+  msdev -ex ExportExampleMakefiles
+)
+
+nmake /nologo /f encoder.mak CFG="encoder - Win32 Debug" %1
+nmake /nologo /f encoder.mak CFG="encoder - Win32 Release" %1
+nmake /nologo /f encoder_static.mak CFG="encoder_static - Win32 Debug" %1
+nmake /nologo /f encoder_static.mak CFG="encoder_static - Win32 Release" %1
+nmake /nologo /f decoder.mak CFG="decoder - Win32 Debug" %1
+nmake /nologo /f decoder.mak CFG="decoder - Win32 Release" %1
+nmake /nologo /f decoder_static.mak CFG="decoder_static - Win32 Debug" %1
+nmake /nologo /f decoder_static.mak CFG="decoder_static - Win32 Release" %1
+nmake /nologo /f chaining.mak CFG="chaining - Win32 Debug" %1
+nmake /nologo /f chaining.mak CFG="chaining - Win32 Release" %1
+nmake /nologo /f chaining_static.mak CFG="chaining_static - Win32 Debug" %1
+nmake /nologo /f chaining_static.mak CFG="chaining_static - Win32 Release" %1
+nmake /nologo /f seeking.mak CFG="seeking - Win32 Debug" %1
+nmake /nologo /f seeking.mak CFG="seeking - Win32 Release" %1
+nmake /nologo /f seeking_static.mak CFG="seeking_static - Win32 Debug" %1
+nmake /nologo /f seeking_static.mak CFG="seeking_static - Win32 Release" %1
+nmake /nologo /f vorbisfile.mak CFG="vorbisfile - Win32 Debug" %1
+nmake /nologo /f vorbisfile.mak CFG="vorbisfile - Win32 Release" %1
+nmake /nologo /f vorbisfile_static.mak CFG="vorbisfile_static - Win32 Debug" %1
+nmake /nologo /f vorbisfile_static.mak CFG="vorbisfile_static - Win32 Release" %1
+
+goto normal
+
+:nolib
+echo ***** SDK needs to be built first 
+exit
+
+:notset
+echo ***** Error: must set SDKHOME
+exit
+
+:normal
---
+++ win32sdk/sdk/build/clean_all.bat
@@ -0,0 +1,5 @@
+rem
+rem $Id: clean_all.bat,v 1.1 2001/09/14 01:53:24 cwolf Exp $
+rem Call the make clean targets for each example program target
+rem
+call build_all.bat CLEAN
