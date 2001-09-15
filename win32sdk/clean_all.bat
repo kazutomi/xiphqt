778d9873334074c256afd9e7aa5aa9edd44d49ed
cwolf <cwolf@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2001-09-15 07:03:06 +0000

    Suppress spurious waring messages

--- win32sdk/clean_all.bat
+++ win32sdk/clean_all.bat
@@ -1,5 +1,6 @@
+@echo off
 rem
-rem $Id: clean_all.bat,v 1.1 2001/09/13 19:55:00 cwolf Exp $
+rem $Id: clean_all.bat,v 1.2 2001/09/15 07:03:06 cwolf Exp $
 rem Call the make clean targets in each respective modules' subdirectory
 rem
-call build_all.bat CLEAN
+call build_all.bat CLEAN 2> nul
