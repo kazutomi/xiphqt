667f6dce24cd15ab06352709a56789506ff6c5dc
cwolf <cwolf@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2001-10-20 17:30:24 +0000

    typo

--- win32sdk/clean_libs_mak.bat
+++ win32sdk/clean_libs_mak.bat
@@ -1,9 +1,9 @@
-@echo off
-rem $Id: clean_libs_mak.bat,v 1.1 2001/09/15 08:09:23 cwolf Exp $
+rem @echo off
+rem $Id: clean_libs_mak.bat,v 1.2 2001/10/20 17:30:24 cwolf Exp $
 rem
 rem Deletes auto generated makefiles in ogg/win32 and vorbis/win32
 rem
-if ."%SRCROOT%"=="". goto notset
+if ."%SRCROOT%"==."" goto notset
 del %SRCROOT%\ogg\win32\*.mak 2> nul
 del %SRCROOT%\ogg\win32\*.dep 2> nul
 del %SRCROOT%\vorbis\win32\*.mak 2> nul
