c1ade198a0ea8b47da1b10a9a9e6eed252c8d779
andrew <andrew@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2002-01-27 11:18:23 +0000

    Bump version to 0.5 and fix Changelog dates.

--- vorbis-python/setup.py
+++ vorbis-python/setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 from distutils.extension import Extension
 
 VERSION_MAJOR = 0
-VERSION_MINOR = 4
+VERSION_MINOR = 5
 pyvorbis_version = str(VERSION_MAJOR) + '.' + str(VERSION_MINOR)
 
 try:
