8c8789da720ac0eceaa3e6c77a1fa350838c2edb
jack <jack@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2003-05-21 18:48:57 +0000

    Add BSD-style license and sample header to positron.py.

--- positron/src/positron.py
+++ positron/src/positron.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+# -*- Mode: python -*-
+#
+# positron.py - main frontend
+#
+# Copyright (C) 2003, Xiph.org Foundation
+#
+# This file is part of positron.
+#
+# This program is free software; you can redistribute it and/or modify it 
+# under the terms of a BSD-style license (see the COPYING file in the 
+# distribution).
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the license for more details.
+
 import sys
 from os import path
 import getopt
