9872610384332a8d4c2531f0e5e369d71bb33f4c
jack <jack@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2003-05-22 03:28:43 +0000

    First draft of setup.py.  Probably doesn't work yet.

---
+++ positron/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- Mode: python -*-
+#
+# setup.py - distutils setup script
+#
+# Copyright (C) 2003, Xiph.org Foundation
+#
+# This file is part of positron.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of a BSD-style license (see the COPYING file in the
+# distribution).
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the license for more details.
+
+from distutils.core import setup
+
+setup(name="positron",
+      version="1.0b1",
+      summary="Positron synchronization software for the Neuros Audio Computer",
+      platform=["POSIX"],
+      license="BSD",
+      author="Stan Seibert",
+      author_email="volsung@xiph.org",
+      url="http://www.neurosaudio.com/",
+      package_dir = {'positron': 'src'},
+      scripts=['scripts/positron']
+      )
+
+
