cd7df3d53386d1afcb05ffd41ec09fcde81d860f
volsung <volsung@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2003-07-08 02:59:37 +0000

    Apparently using smaller block sizes helps people who frequently have USB errors when syncing.  Hopefully mitigates bug 367 somewhat.

--- positron/positron/util.py
+++ positron/positron/util.py
@@ -31,7 +31,7 @@ def copy_file (src_filename, dest_filename):
     """Copy a file from src_filename to dest_filename
 
     Directories are created as needed to accomodate dest_filename"""
-    blocksize = 1048576
+    blocksize = 1024
     src = file(src_filename, "rb")
 
     (dirname, basename) = os.path.split(dest_filename)
