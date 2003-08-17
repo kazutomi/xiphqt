2d205127308030edb2dfc6b3e09834d223b0cd2b
arc <arc@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2003-08-17 21:16:14 +0000

    *** empty log message ***

--- writ/include/writ/writ.h
+++ writ/include/writ/writ.h
@@ -11,13 +11,13 @@
  ********************************************************************
 
  function: toplevel libwrit include
- last mod: $Id: writ.h,v 1.2 2003/08/17 08:34:49 arc Exp $
+ last mod: $Id: writ.h,v 1.3 2003/08/17 21:16:13 arc Exp $
 
  ********************************************************************/
 
 #include <ogg/ogg.h>
 
-
+  
 typedef struct writ_text {
   int    length;
   char  *string;
@@ -32,20 +32,23 @@ typedef struct writ_langdef {
 
 typedef struct writ_info {
   int           version;
-
+  
   ogg_uint32_t  granulerate_numerator;
   ogg_uint32_t  granulerate_denominator;
-
+  
   ogg_uint16_t  location_scale_x;
   ogg_uint16_t  location_scale_y;
-
+  
   int           num_languages;
   writ_langdef *languages;
 } writ_info;
 
-  
+
 typedef struct writ_state {
   writ_info    *wi;
+  
+  int          phrases_buffed;
+  writ_phrase  *phrase_buff;
   
   ogg_int64_t granulepos;
 } writ_state;
@@ -54,15 +57,15 @@ typedef struct writ_state {
 typedef struct writ_phrase {
   ogg_int64_t   granulepos;
   ogg_uint32_t  duration;
-
+  
   ogg_uint16_t  location_x;
   ogg_uint16_t  location_y;
   ogg_uint16_t  location_width;
   ogg_uint16_t  location_height;
-
+  
   int	        alignment_x; 		/* 0=left, 1=right, 2=center, 3=full */
   int           alignment_y;		/* 0=top, 1=bottom, 2=middle, 3=full */
-
+  
   writ_text    *phrase;
 } writ_phrase;  
 
