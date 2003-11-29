a589ec876509dd591817928c782d04da6b77d5cb
arc <arc@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2003-11-29 14:50:58 +0000

    Alpha 0; forgot this one

---
+++ ogg-python2/include/pyogg/pyogg2.h
@@ -0,0 +1,28 @@
+#ifndef __PYOGG_H__
+#define __PYOGG_H__
+
+#include <Python.h>
+#include <ogg2/ogg.h>
+
+/* The struct to hold everything we pass from the _ogg module
+ * to its submodules.
+ */
+
+typedef struct {
+  int version_major;
+  int version_minor;
+
+  PyTypeObject *PyOggPacket_Type;
+  PyTypeObject *PyOggStreamState_Type;
+  PyObject *PyOggError;
+  PyObject *(*PyOggPacket_From_Packet)(ogg_packet *packet);
+  int (*arg_to_int64)(PyObject *longobj, ogg_int64_t *val);
+} ogg2_module_info;
+
+/*
+  Function to convert Long Python objects into an ogg_int64 value.
+  Returns 0 on failure (a Python error will be set)
+*/
+int arg_to_int64(PyObject *longobj, ogg_int64_t *val);
+
+#endif // __PYOGG_H__
