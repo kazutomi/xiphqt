c0acb19df8458f1bfe157f682b51a9e7c85f9957
arc <arc@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2004-03-02 09:09:35 +0000

    This is for the module API.

---
+++ dryice/include/dryice/dryice.h
@@ -0,0 +1,14 @@
+/*  dryice.h
+**
+**  module API for DryIce, a live Ogg stream source client
+*/
+
+typedef struct {
+  int frame_width;  
+  int frame_height;  
+  int framerate_numerator;  
+  int framerate_denominator;  
+
+
+} dryice_video_params;
+
