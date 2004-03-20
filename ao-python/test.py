8957526171fca8313af109034fb73c8fd9ff99ef
unknown <unknown@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2004-03-20 18:50:40 +0000

    This commit was manufactured by cvs2svn to create branch 'vp32'.

--- ao-python/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import ao
-
-dev = ao.AudioDevice('wav', bits=16)
-f = open('test.wav', 'r')
-data = f.read()
-dev.play(data, len(data))
-
--- win32-tools/oggdrop/encthread.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ENCTHREAD_H__
-#define __ENCTHREAD_H__
-
-void encthread_init(void);
-void encthread_addfile(char *file);
-void encthread_setbitrate(int kbps);
-
-#endif
