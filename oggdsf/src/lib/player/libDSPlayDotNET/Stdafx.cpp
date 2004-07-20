fe440d0a36ff8063e471077cc703913a84b32cc4
arc <arc@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2004-07-20 19:23:29 +0000

    Initial branch.
    Tackles bugs 484, 544, and de-exposed ogg_page_checksum_set for the
    reasoning detailed in bug 485.
    
    Also updated some docs and comments to read that the repository is now
    in subversion (svn) instead of cvs and updated the copyright notices of
    updated files for 2004.

--- oggdsf/src/lib/player/libDSPlayDotNET/Stdafx.cpp
+++
@@ -1,6 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// libDSPlayDotNET.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-// {6188AD0C-62CB-4658-A14E-CD23CF84EC31}
