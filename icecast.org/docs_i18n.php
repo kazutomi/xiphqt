7768c4dece680bac4cc7b645f806ae194f3d1417
pem <pem@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2005-01-09 01:15:29 +0000

    A docs.php page prepared for translations

---
+++ icecast.org/docs_i18n.php
@@ -0,0 +1,44 @@
+<? include "common/header.php"; ?>
+<h2>Documentation and Information</h2>
+<div class="roundcont">
+<div class="roundtop">
+<img src="/images/corner_topleft.jpg" class="corner" style="display: none" />
+</div>
+<div class="newscontent">
+<h3>Official Documentation</h3>
+
+    <p>We are translating the documentation. We need your help. We need translators 
+      and volunteers to read again an already translated one. If you're intereste, 
+      send an email to translation@icecast.org.</p>
+    <p>At the moment, the documentation is available in :</p>
+    <p><img src="images/uk.gif" width="30" height="18"> English | <img src="images/france.gif" width="31" height="19"> 
+      French</p>
+    <p>You can still read the documentation for older versions or other icecast 
+      project softwares <a href="docs/">now available online</a>. </p>
+    <p>Also, we have a simple <a href="faq.php">FAQ</a>.</p>
+
+<h3>Other Information</h3>
+    <p>Here is an index to the icecast documentation we know about. If there is something 
+      out ther on the Web that you know about, but is not listed here, send an 
+      email to webmaster@icecast.org. </p>
+    <p>* <a href="http://www.6809.org.uk/kja3/ices2-howto.shtml">Ciaran Anscomb's 
+      IceS2 HOWTO</a> </p>
+<p>
+* <a href="http://melmoth.dyndns.org/stream/">Pierre Amadio's Quelques notes éparses sur l'installation d'Icecast 2 sur une Woody</a>
+</p>
+<p>
+* <a href="http://www.hanfbu.de/downloads/linux/howto/streaming.pdf">German icecast2 and ices2 howto written by Tobias Haf.</a>
+</p>
+<p>
+* <a href="http://www.gnuware.com/icecast/">icecast howto written by Kerry Cox.</a>
+</p>
+<p>
+* <a href="http://rezal404.org/wikini/wakka.php?wiki=IceCast2ToC">French translation of Icecast documentation - by Nicolas Henry</a>
+</p>
+</div>
+<div class="roundbottom">
+<img src="/images/corner_bottomleft.jpg" class="corner" style="display: none" />
+</div>
+</div>
+<br><br>
+<? include "common/footer.php"; ?>
