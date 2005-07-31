913a06980d288001054141e93ba3ceddec88d86f
xiphmont <xiphmont@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2005-07-31 06:41:55 +0000

    Oh, forgot to set keywords

--- planarity/version.h
+++ planarity/version.h
@@ -1,2 +1,2 @@
-#define VERSION "$Id: version.h 8581 2005-01-03 03:41:05Z xiphmont $ "
-/* DO NOT EDIT: Automated versioning hack [Sun Jul 31 02:39:35 EDT 2005] */
+#define VERSION "$Id$ "
+/* DO NOT EDIT: Automated versioning hack [Sun Jul 31 02:40:23 EDT 2005] */
