0a004104a20978f5edb37bba9ded7db11c6ffb9c
illiminable <illiminable@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2005-10-22 13:38:30 +0000

    * New interface for all decode filters

---
+++ src/lib/core/directshow/dsfOggDemux2/IOggDecoder.h
@@ -0,0 +1,18 @@
+#pragma once
+class IOggDecoder 
+{
+public:
+	enum eAcceptHeaderResult {
+		AHR_ALL_HEADERS_RECEIVED,
+		AHR_MORE_HEADERS_TO_COME,
+		AHR_INVALID_HEADER,
+		AHR_UNEXPECTED,
+		AHR_NULL_POINTER,
+
+	};
+	virtual LOOG_INT64 convertGranuleToTime(LOOG_INT64 inGranule) = 0;
+	virtual eAcceptHeaderResult acceptHeaderPage(OggPage* inOggHeaderPage) = 0;
+	virtual string getCodecShortName() = 0;
+	virtual string getCodecIdentString() = 0;
+	
+};
