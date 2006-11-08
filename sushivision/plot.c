35dd8de0afc4a660fc2e58df7d3a9d93fc8c9aef
xiphmont <xiphmont@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2006-11-08 12:29:06 +0000

    Correct a rubberbanding expose bug at high zoom

--- sushivision/plot.c
+++ sushivision/plot.c
@@ -408,8 +408,8 @@ static void box_check(Plot *p, int x, int y){
     plot_expose_request_partial(p,
 				(int)(vals[0]),
 				(int)(vals[1]),
-				(int)(vals[2]+2),
-				(int)(vals[3]+2));
+				(int)(vals[2]+3), // account for floor/ceil *and* roundoff potential
+				(int)(vals[3]+3));
   }
 }
 
@@ -433,8 +433,8 @@ static gint mouse_motion(GtkWidget        *widget,
       plot_expose_request_partial(p,
 				  (int)(vals[0]),
 				  (int)(vals[1]),
-				  (int)(vals[2]+2),
-				  (int)(vals[3]+2));
+				  (int)(vals[2]+3),  // account for floor/ceil *and* roundoff potential
+				  (int)(vals[3]+3));
     }
     
     p->box_x2 = scalespace_value(&p->x,x);
