5275eb5166d3c952860f01e42714f6daea7ea7c2
bcohee <bcohee@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2007-08-08 23:47:16 +0000

    Edited license header

--- vorbis-java/org/xiph/libshout/ShoutClient.java
+++ vorbis-java/org/xiph/libshout/ShoutClient.java
@@ -1,14 +1,16 @@
 /********************************************************************
  *                                                                  *
- * THIS FILE IS PART OF THE OggVorbis SOFTWARE CODEC SOURCE CODE.   *
- * USE, DISTRIBUTION AND REPRODUCTION OF THIS LIBRARY SOURCE IS     *
- * GOVERNED BY A BSD-STYLE SOURCE LICENSE INCLUDED WITH THIS SOURCE *
- * IN 'COPYING'. PLEASE READ THESE TERMS BEFORE DISTRIBUTING.       *
+ * Java software implementation library for communicating with and  *
+ * sending data to an icecast server.                               *
+ *                                                                  *
+ * Credit:                                                          *
+ *         http://www.icecast.org                                   *
  *                                                                  *
  * THE OggVorbis SOURCE CODE IS (C) COPYRIGHT 1994-2002             *
  * by the Xiph.Org Foundation http://www.xiph.org/                  *
  *                                                                  *
  ********************************************************************/
+
 
 package org.xiph.libshout;
 
