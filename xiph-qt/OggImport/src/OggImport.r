85cd1840791fcc197e8a05e0e17cbbec1d12d234
arek <arek@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2007-09-01 15:09:39 +0000

    * OggImport/src/OggImport.r: 	Add the new proposed file extensions and MIME types (from 	http://wiki.xiph.org/index.php/MIME_Types_and_File_Extensions) to 	the OggImport component.

--- xiph-qt/OggImport/src/OggImport.r
+++ xiph-qt/OggImport/src/OggImport.r
@@ -213,6 +213,115 @@ resource 'thga' (kImporterResID + 3, OggImporterName, purgeable) {
     'thnr', kImporterResID, 0
 };
 
+// support the new proposed file extensions, see:
+// http://wiki.xiph.org/index.php/MIME_Types_and_File_Extensions
+
+//   .ogx - Ogg Multiplex Profile (anything in Ogg)
+resource 'thga' (kImporterResID + 4, OggImporterName, purgeable) {
+    kImporterComponentType,             // Type
+    'OGX ',                             // Subtype
+    'vide',                             // Manufacturer
+    kImporterFlags | movieImportSubTypeIsFileExtension,
+    0,                                  // Component Flags Mask
+    0, 0,                               // Code Type, Code ID
+    'STR ',                             // Name Type
+    kImporterNameStringResID,           // Name ID
+    'STR ',                             // Info Type
+    kImporterInfoStringResID,           // Info ID
+    0, 0,                               // Icon Type, Icon ID
+                // TARGET COMPONENT ---------------
+    kImporterComponentType,             // Type
+    kCodecFormat,                       // SubType
+    'vide',                             // Manufaturer
+    kImporterFlags, 0,                  // Component Flags and Mask
+    'thnr', kImporterResID, 0
+};
+
+//   .oga - Ogg Audio Profile (audio in Ogg container)
+resource 'thga' (kImporterResID + 5, OggImporterName, purgeable) {
+    kImporterComponentType,             // Type
+    'OGA ',                             // Subtype
+    'vide',                             // Manufacturer
+    kImporterFlags | movieImportSubTypeIsFileExtension,
+    0,                                  // Component Flags Mask
+    0, 0,                               // Code Type, Code ID
+    'STR ',                             // Name Type
+    kImporterNameStringResID,           // Name ID
+    'STR ',                             // Info Type
+    kImporterInfoStringResID,           // Info ID
+    0, 0,                               // Icon Type, Icon ID
+                // TARGET COMPONENT ---------------
+    kImporterComponentType,             // Type
+    kCodecFormat,                       // SubType
+    'vide',                             // Manufaturer
+    kImporterFlags, 0,                  // Component Flags and Mask
+    'thnr', kImporterResID, 0
+};
+
+//   .ogv - Ogg Video Profile (a/v in Ogg container)
+resource 'thga' (kImporterResID + 6, OggImporterName, purgeable) {
+    kImporterComponentType,             // Type
+    'OGV ',                             // Subtype
+    'vide',                             // Manufacturer
+    kImporterFlags | movieImportSubTypeIsFileExtension,
+    0,                                  // Component Flags Mask
+    0, 0,                               // Code Type, Code ID
+    'STR ',                             // Name Type
+    kImporterNameStringResID,           // Name ID
+    'STR ',                             // Info Type
+    kImporterInfoStringResID,           // Info ID
+    0, 0,                               // Icon Type, Icon ID
+                // TARGET COMPONENT ---------------
+    kImporterComponentType,             // Type
+    kCodecFormat,                       // SubType
+    'vide',                             // Manufaturer
+    kImporterFlags, 0,                  // Component Flags and Mask
+    'thnr', kImporterResID, 0
+};
+
+//   .axa - Profile for audio in Annodex
+resource 'thga' (kImporterResID + 7, OggImporterName, purgeable) {
+    kImporterComponentType,             // Type
+    'AXA ',                             // Subtype
+    'vide',                             // Manufacturer
+    kImporterFlags | movieImportSubTypeIsFileExtension,
+    0,                                  // Component Flags Mask
+    0, 0,                               // Code Type, Code ID
+    'STR ',                             // Name Type
+    kImporterNameStringResID,           // Name ID
+    'STR ',                             // Info Type
+    kImporterInfoStringResID,           // Info ID
+    0, 0,                               // Icon Type, Icon ID
+                // TARGET COMPONENT ---------------
+    kImporterComponentType,             // Type
+    kCodecFormat,                       // SubType
+    'vide',                             // Manufaturer
+    kImporterFlags, 0,                  // Component Flags and Mask
+    'thnr', kImporterResID, 0
+};
+
+//   .axv - Profile for video in Annodex
+resource 'thga' (kImporterResID + 8, OggImporterName, purgeable) {
+    kImporterComponentType,             // Type
+    'AXV ',                             // Subtype
+    'vide',                             // Manufacturer
+    kImporterFlags | movieImportSubTypeIsFileExtension,
+    0,                                  // Component Flags Mask
+    0, 0,                               // Code Type, Code ID
+    'STR ',                             // Name Type
+    kImporterNameStringResID,           // Name ID
+    'STR ',                             // Info Type
+    kImporterInfoStringResID,           // Info ID
+    0, 0,                               // Icon Type, Icon ID
+                // TARGET COMPONENT ---------------
+    kImporterComponentType,             // Type
+    kCodecFormat,                       // SubType
+    'vide',                             // Manufaturer
+    kImporterFlags, 0,                  // Component Flags and Mask
+    'thnr', kImporterResID, 0
+};
+
+
 resource 'thnr' (kImporterResID, OggImporterName, purgeable) {
     {
         'mime', 1, 0, 'mime', kImporterResID, cmpResourceNoFlags,
@@ -242,21 +351,56 @@ resource 'STR ' (kImporterInfoStringResID, OggImporterName, purgeable) {
 */
 resource 'mime' (kImporterResID, OggImporterName, purgeable) {
     {
-        kMimeInfoMimeTypeTag,      1, "application/ogg";
-        kMimeInfoMimeTypeTag,      2, "application/x-ogg";
-        kMimeInfoMimeTypeTag,      3, "audio/x-speex";
-        kMimeInfoMimeTypeTag,      4, "audio/speex";
-        kMimeInfoMimeTypeTag,      5, "application/x-annodex";
-        kMimeInfoFileExtensionTag, 1, "ogg";
-        kMimeInfoFileExtensionTag, 2, "ogg";
-        kMimeInfoFileExtensionTag, 3, "spx";
-        kMimeInfoFileExtensionTag, 4, "spx";
-        kMimeInfoFileExtensionTag, 5, "anx";
-        kMimeInfoDescriptionTag,   1, "Ogg Multimedia Bitstream";
-        kMimeInfoDescriptionTag,   2, "Ogg Multimedia Bitstream";
-        kMimeInfoDescriptionTag,   3, "Ogg Speex";
-        kMimeInfoDescriptionTag,   4, "Ogg Speex";
-        kMimeInfoDescriptionTag,   5, "Annodex Media";
+        kMimeInfoMimeTypeTag,       1, "application/ogg";
+        kMimeInfoMimeTypeTag,       2, "application/x-ogg";
+        kMimeInfoMimeTypeTag,       3, "audio/x-speex";
+        kMimeInfoMimeTypeTag,       4, "audio/speex";
+        kMimeInfoMimeTypeTag,       5, "application/x-annodex";
+        kMimeInfoMimeTypeTag,       6, "application/annodex";
+        kMimeInfoMimeTypeTag,       7, "application/ogg";
+        kMimeInfoMimeTypeTag,       8, "application/x-ogg";
+        kMimeInfoMimeTypeTag,       9, "audio/x-ogg";
+        kMimeInfoMimeTypeTag,      10, "audio/ogg";
+        kMimeInfoMimeTypeTag,      11, "video/x-ogg";
+        kMimeInfoMimeTypeTag,      12, "video/ogg";
+        kMimeInfoMimeTypeTag,      13, "audio/x-annodex";
+        kMimeInfoMimeTypeTag,      14, "audio/annodex";
+        kMimeInfoMimeTypeTag,      15, "video/x-annodex";
+        kMimeInfoMimeTypeTag,      16, "video/annodex";
+
+        kMimeInfoFileExtensionTag,  1, "ogg";
+        kMimeInfoFileExtensionTag,  2, "ogg";
+        kMimeInfoFileExtensionTag,  3, "spx";
+        kMimeInfoFileExtensionTag,  4, "spx";
+        kMimeInfoFileExtensionTag,  5, "anx";
+        kMimeInfoFileExtensionTag,  6, "anx";
+        kMimeInfoFileExtensionTag,  7, "ogx";
+        kMimeInfoFileExtensionTag,  8, "ogx";
+        kMimeInfoFileExtensionTag,  9, "oga";
+        kMimeInfoFileExtensionTag, 10, "oga";
+        kMimeInfoFileExtensionTag, 11, "ogv";
+        kMimeInfoFileExtensionTag, 12, "ogv";
+        kMimeInfoFileExtensionTag, 13, "axa";
+        kMimeInfoFileExtensionTag, 14, "axa";
+        kMimeInfoFileExtensionTag, 15, "axv";
+        kMimeInfoFileExtensionTag, 16, "axv";
+
+        kMimeInfoDescriptionTag,    1, "Ogg Multimedia Bitstream";
+        kMimeInfoDescriptionTag,    2, "Ogg Multimedia Bitstream";
+        kMimeInfoDescriptionTag,    3, "Ogg Speex";
+        kMimeInfoDescriptionTag,    4, "Ogg Speex";
+        kMimeInfoDescriptionTag,    5, "Annodex Media";
+        kMimeInfoDescriptionTag,    6, "Annodex Media";
+        kMimeInfoDescriptionTag,    7, "Ogg Multimedia Bitstream";
+        kMimeInfoDescriptionTag,    8, "Ogg Multimedia Bitstream";
+        kMimeInfoDescriptionTag,    9, "Ogg Audio";
+        kMimeInfoDescriptionTag,   10, "Ogg Audio";
+        kMimeInfoDescriptionTag,   11, "Ogg Video";
+        kMimeInfoDescriptionTag,   12, "Ogg Video";
+        kMimeInfoDescriptionTag,   13, "Annodex Audio";
+        kMimeInfoDescriptionTag,   14, "Annodex Audio";
+        kMimeInfoDescriptionTag,   15, "Annodex Video";
+        kMimeInfoDescriptionTag,   16, "Annodex Video";
     };
 };
 
@@ -281,7 +425,7 @@ resource 'mcfg' (kImporterResID, OggImporterName, purgeable) {
 
         {
             "Ogg Multimedia Bitstream",
-            "ogg",
+            "ogg, ogx",
             "QuickTime Player",
             "Ogg file importer",
             "",
@@ -291,6 +435,68 @@ resource 'mcfg' (kImporterResID, OggImporterName, purgeable) {
         {
             "application/x-ogg";
             "application/ogg";
+        };
+//    };
+//    {
+        kQTMediaConfigVideoGroupID,
+        kQTMediaConfigBinaryFile | \
+            kQTMediaConfigTakeFileAssociationByDefault | \
+            kQTMediaConfigCanUseApp | kQTMediaConfigCanUsePlugin | \
+            kQTMediaConfigUsePluginByDefault,
+        'OggS',
+        'TVOD',	/* we don't have a creator code for our files, hijack QT player */
+        kImporterComponentType, kCodecFormat, kSoundComponentManufacturer,
+        0, 0,
+        'OGV ',
+        kQTMediaInfoNetGroup,
+
+        /* no synonyms */
+        {
+        },
+
+        {
+            "Ogg Video",
+            "ogv",
+            "QuickTime Player",
+            "Ogg Video file importer",
+            "",
+        },
+
+        /* mime types array */
+        {
+            "video/x-ogg";
+            "video/ogg";
+        };
+//    };
+//    {
+        kQTMediaConfigAudioGroupID,
+        kQTMediaConfigBinaryFile | \
+            kQTMediaConfigTakeFileAssociationByDefault | \
+            kQTMediaConfigCanUseApp | kQTMediaConfigCanUsePlugin | \
+            kQTMediaConfigUsePluginByDefault,
+        'OggS',
+        'TVOD',	/* we don't have a creator code for our files, hijack QT player */
+        kImporterComponentType, kCodecFormat, kSoundComponentManufacturer,
+        0, 0,
+        'OGA ',
+        kQTMediaInfoNetGroup,
+
+        /* no synonyms */
+        {
+        },
+
+        {
+            "Ogg Audio",
+            "oga",
+            "QuickTime Player",
+            "Ogg Audio file importer",
+            "",
+        },
+
+        /* mime types array */
+        {
+            "audio/x-ogg";
+            "audio/ogg";
         };
 //    };
 //    {
@@ -352,6 +558,69 @@ resource 'mcfg' (kImporterResID, OggImporterName, purgeable) {
         /* mime types array */
         {
             "application/x-annodex";
+            "application/annodex";
+        };
+//    };
+//    {
+        kQTMediaConfigAudioGroupID,
+        kQTMediaConfigBinaryFile | \
+            kQTMediaConfigTakeFileAssociationByDefault | \
+            kQTMediaConfigCanUseApp | kQTMediaConfigCanUsePlugin | \
+            kQTMediaConfigUsePluginByDefault,
+        'OggS',
+        'TVOD',	/* we don't have a creator code for our files, hijack QT player */
+        kImporterComponentType, kCodecFormat, kSoundComponentManufacturer,
+        0, 0,
+        'AXA ',
+        kQTMediaInfoNetGroup,
+
+        /* no synonyms */
+        {
+        },
+
+        {
+            "Annodex Audio",
+            "axa",
+            "QuickTime Player",
+            "Annodex Audio file importer",
+            "",
+        },
+
+        /* mime types array */
+        {
+            "audio/x-annodex";
+            "audio/annodex";
+        };
+//    };
+//    {
+        kQTMediaConfigVideoGroupID,
+        kQTMediaConfigBinaryFile | \
+            kQTMediaConfigTakeFileAssociationByDefault | \
+            kQTMediaConfigCanUseApp | kQTMediaConfigCanUsePlugin | \
+            kQTMediaConfigUsePluginByDefault,
+        'OggS',
+        'TVOD',	/* we don't have a creator code for our files, hijack QT player */
+        kImporterComponentType, kCodecFormat, kSoundComponentManufacturer,
+        0, 0,
+        'AXV ',
+        kQTMediaInfoNetGroup,
+
+        /* no synonyms */
+        {
+        },
+
+        {
+            "Annodex Video",
+            "axv",
+            "QuickTime Player",
+            "Annodex Video file importer",
+            "",
+        },
+
+        /* mime types array */
+        {
+            "video/x-annodex";
+            "video/annodex";
         };
     };
 };
