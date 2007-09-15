0dd5790cef3fdce6b82c4bf2eb7dc93dd5f54c63
andre.lnc <andre.lnc@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2007-09-15 22:10:43 +0000

    Add the LEON3 and update the path sctruct

---
+++ theora-fpga/testbenchs/libtheora_tb/lib/olimpiada.h
@@ -0,0 +1,44 @@
+#define VEZES_LF 15
+#define VEZES_EB 15
+#define VEZES_RF 15
+#define VEZES_CR 15
+#define VEZES_RR 15
+
+
+/*#define LOOP_FILTER 1*/
+/* #define EXP_BLK 1 */
+/*#define RECON_FRAMES 1*/
+/* #define COPY_RECON 1 */
+#define RECON_REF_FRAMES 1
+/* #define RECON_PIXEL_INDEX 1 */
+
+
+
+#define IN_EB_FILE "/home/leonardo/theora/modulos/expandblock/golden/in.tb"
+#define IN2_EB_FILE "/home/leonardo/theora/modulos/expandblock/golden/in2.tb"
+#define OUT_EB_FILE "/home/leonardo/theora/modulos/expandblock/golden/OUT.EXPECTED.TB"
+
+
+#define IN_LF_FILE "/home/leonardo/theora/modulos/loopfilter/golden/in.tb"
+#define IN2_LF_FILE "/home/leonardo/theora/modulos/loopfilter/golden/in2.tb"
+#define OUT_LF_FILE "/home/leonardo/theora/modulos/loopfilter/golden/OUT.EXPECTED.TB"
+
+
+#define IN_RF_FILE  "/home/leonardo/theora/modulos/reconframes/golden/in.tb"
+#define IN2_RF_FILE "/home/leonardo/theora/modulos/reconframes/golden/in2.tb"
+#define OUT_RF_FILE "/home/leonardo/theora/modulos/reconframes/golden/OUT.EXPECTED.TB"
+
+#define IN_CR_FILE  "/home/leonardo/theora/modulos/copyrecon/golden/in.tb"
+#define IN2_CR_FILE "/home/leonardo/theora/modulos/copyrecon/golden/in2.tb"
+#define OUT_CR_FILE "/home/leonardo/theora/modulos/copyrecon/golden/OUT.EXPECTED.TB"
+
+#define IN_RR_FILE  "/home/leonardo/theora/modulos/reconrefframes/golden/in.tb"
+#define OUT_RR_FILE "/home/leonardo/theora/modulos/reconrefframes/golden/OUT.EXPECTED.TB"
+
+#define IN_RPI_FILE  "/home/leonardo/theora/modulos/reconpixelindex/golden/in.tb"
+#define OUT_RPI_FILE "/home/leonardo/theora/modulos/reconpixelindex/golden/OUT.EXPECTED.TB"
+
+
+#define MAXCOUNT 1100
+
+#define START_RF -1
