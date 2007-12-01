eb8769ee38685482da0318553be3c50150942708
moritz <moritz@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2007-12-01 21:39:44 +0000

    Tag the 0.5.3 release.

---
+++ ezstream-0.5.3/win32/config.h
@@ -0,0 +1,40 @@
+#include <sys/types.h>
+#include <windows.h>
+#include <io.h>
+
+#define HAVE_INTTYPES_H 	1
+#define HAVE_LOCALE_H		1
+#define HAVE_STAT		1
+#define HAVE_STDINT_H		1
+#define HAVE_STRUCT_TIMEVAL	1
+#define HAVE_SYS_STAT_H 	1
+
+#define ICONV_CONST		const
+
+/* Name of package */
+#define PACKAGE "ezstream"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "https://trac.xiph.org/newticket?component=ezstream"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "ezstream"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "ezstream 0.5.3"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "ezstream"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.5.3"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.5.3"
+
+#ifdef XALLOC_DEBUG
+typedef long ssize_t;
+#endif
