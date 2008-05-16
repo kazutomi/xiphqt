aa20041f731a72def98f05db759ffb74bc209e28
xiphmont <xiphmont@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2008-05-16 05:32:08 +0000

    tag the prerelease

--- ghost/scripts/dump_vectors.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-echo '<Vector<ObjectRef> ' > nbands.vec; cat $* | ../libghost/testghost /dev/stdin /dev/null | sed -e 's/^/<Vector<float> /' -e 's/$/>/' >> nbands.vec; echo ' > ' >> nbands.vec
