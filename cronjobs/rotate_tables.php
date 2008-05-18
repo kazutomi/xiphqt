9df673eabb9e9b8d13df3af0e94003a2fcdcf5d5
balbinus <balbinus@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2008-05-18 21:44:13 +0000

    Bug fixes on previous commit + new table

--- cronjobs/rotate_tables.php
+++ cronjobs/rotate_tables.php
@@ -17,33 +17,91 @@ $yesterday = makeFutureDate('Ymd', -1);
 /******************************************************************************/
 try
 {
+    printf("Creating logged_at index on api_log_%s... ", $yesterday);
+    $start = microtime(true);
     $sql = 'CREATE INDEX `logged_at` ON `api_log_%s` (`logged_at`);';
     $sql = sprintf($sql, $yesterday);
     $db->query($sql);
-    $sql = 'CREATE INDEX `message` ON `api_log_%s` (`message`, `logged_at`);'
+    printf("OK [%.3f s]\n", microtime(true) - $start);
+    
+    printf("Creating message index on api_log_%s... ", $yesterday);
+    $start = microtime(true);
+    $sql = 'CREATE INDEX `message` ON `api_log_%s` (`message`, `logged_at`);';
     $sql = sprintf($sql, $yesterday);
     $db->query($sql);
+    printf("OK [%.3f s]\n", microtime(true) - $start);
 }
 catch (SQLException $e)
 {
+    echo "[ERROR]\n";
+    
     // Mail the error...
     custom_exception_handler($e);
     
     // do nothing.
 }
 
+echo "Creating future tables... ";
 $sql_pattern = 'CREATE TABLE IF NOT EXISTS `api_log_%s` LIKE `api_log_template`;';
 for ($i = 0 ; $i <= 7 ; $i++)
 {
     $date = makeFutureDate('Ymd', $i);
+    echo $date." ";
     
     $sql = sprintf($sql_pattern, $date);
     try
     {
         $db->query($sql);
+        echo "[OK]... ";
     }
     catch (SQLException $e)
     {
+        echo "[ERROR] ! ";
+        // Mail the error...
+        custom_exception_handler($e);
+        
+        // do nothing.
+    }
+}
+
+/******************************************************************************/
+/**                              PLAYLIST LOG                                **/
+/******************************************************************************/
+try
+{
+    printf("Creating stream_name_hash index on playlist_log_%s... ", $yesterday);
+    $start = microtime(true);
+    $sql = 'CREATE INDEX `stream_name_hash` ON `playlist_log_%s` (`stream_name_hash`);';
+    $sql = sprintf($sql, $yesterday);
+    $db->query($sql);
+    printf("OK [%.3f s]\n", microtime(true) - $start);
+}
+catch (SQLException $e)
+{
+    echo "[ERROR]\n";
+    
+    // Mail the error...
+    custom_exception_handler($e);
+    
+    // do nothing.
+}
+
+echo "Creating future tables... ";
+$sql_pattern = 'CREATE TABLE IF NOT EXISTS `playlist_log_%s` LIKE `playlist_log_template`;';
+for ($i = 0 ; $i <= 7 ; $i++)
+{
+    $date = makeFutureDate('Ymd', $i);
+    echo $date." ";
+    
+    $sql = sprintf($sql_pattern, $date);
+    try
+    {
+        $db->query($sql);
+        echo "[OK]... ";
+    }
+    catch (SQLException $e)
+    {
+        echo "[ERROR] ! ";
         // Mail the error...
         custom_exception_handler($e);
         
