f53c6255442c7c34195cce3d4f72872bf98c07bb
nithin <nithin@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2008-06-27 10:42:45 +0000

    Adding locate file dialogue

--- oggPusher/firefox-extension/chrome/content/op_Window.js
+++ oggPusher/firefox-extension/chrome/content/op_Window.js
@@ -1,4 +1,17 @@
-function onClose()
-{
-	window.close();
+var opwindowCommon={
+	openFileWindowDialog:function()
+        {
+                const nsIFilePicker = Components.interfaces.nsIFilePicker;
+                var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
+                fp.init(window, "Choose a File", nsIFilePicker.modeOpen);
+                fp.appendFilters(nsIFilePicker.filterAll | nsIFilePicker.filterText);
+
+                var rv = fp.show();
+                if (rv == nsIFilePicker.returnOK || rv == nsIFilePicker.returnReplace) {
+                	var file = fp.file;
+         		var path = fp.file.path;
+			parent.document.getElementById("file-path").value = path;
+                }
+         }
 }
+
--- oggPusher/firefox-extension/chrome/content/op_browser.js
+++ oggPusher/firefox-extension/chrome/content/op_browser.js
@@ -1,7 +1,42 @@
+var fileName, filePath;
+
 var oggPusherCommon={
 	openDialog:function()
 	{
    		 window.open("chrome://oggPusher/content/op_Window.xul",
                       "oggPusher", "chrome,centerscreen,resizable=yes,toolbar=no,menubar=no");
 	}
+	/*openFileWindowDialog:function()
+	{
+		const nsIFilePicker = Components.interfaces.nsIFilePicker;
+		var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
+		fp.init(window, "Dialog Title", nsIFilePicker.modeOpen);
+		fp.appendFilters(nsIFilePicker.filterAll | nsIFilePicker.filterText);
+
+		var rv = fp.show();
+		if (rv == nsIFilePicker.returnOK || rv == nsIFilePicker.returnReplace) {
+  		var file = fp.file;
+  		// Get the path as string. Note that you usually won't 
+		//   // need to work with the string paths.
+		var path = fp.file.path;
+		// work with returned nsILocalFile...
+		
+		//var textboxElement = document.getElementById("file-path");
+			
+  		//textboxElement.value =
+
+	       	}	
+  	}*/
 }
+
+
+
+function getMainWindow() {
+  return window.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+                   .getInterface(Components.interfaces.nsIWebNavigation)
+                   .QueryInterface(Components.interfaces.nsIDocShellTreeItem)
+                   .rootTreeItem
+                   .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+                   .getInterface(Components.interfaces.nsIDOMWindow);
+}
+
