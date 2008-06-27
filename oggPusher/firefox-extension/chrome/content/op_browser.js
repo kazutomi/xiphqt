83a829c44681676cb7d97048128ce925d95e890e
nithin <nithin@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2008-06-27 00:54:19 +0000

    resolving conflicts with firefox functionalities

--- oggPusher/firefox-extension/chrome/content/op_browser.js
+++ oggPusher/firefox-extension/chrome/content/op_browser.js
@@ -1,9 +1,7 @@
-function openDialog()
-{
-   //alert("hello"); 
-   var object = window.open("chrome://oggPusher/content/op_Window.xul",
+var oggPusherCommon={
+	openDialog:function()
+	{
+   		 window.open("chrome://oggPusher/content/op_Window.xul",
                       "oggPusher", "chrome,centerscreen,resizable=yes,toolbar=no,menubar=no");
-	if(object==null)
-	{ alert("inside the if");	}
+	}
 }
-
