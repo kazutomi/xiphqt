36c31c7456938ec3e31eb02dd8ecb36ae862405c
nithin <nithin@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2008-06-29 00:25:13 +0000

    Adding Upload. But its not functioanal

--- oggPusher/firefox-extension/chrome/content/op_Window.js
+++ oggPusher/firefox-extension/chrome/content/op_Window.js
@@ -1,11 +1,17 @@
 var opwindowCommon={
 	openFileWindowDialog:function()
         {
-                const nsIFilePicker = Components.interfaces.nsIFilePicker;
-                var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
-                fp.init(window, "Choose a File", nsIFilePicker.modeOpen);
-                fp.appendFilters(nsIFilePicker.filterAll | nsIFilePicker.filterText);
-
+		alert("Inside openFileWindowDialog");
+		try{
+                	const nsIFilePicker = Components.interfaces.nsIFilePicker;
+	                var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
+        	        fp.init(window, "Choose a File", nsIFilePicker.modeOpen);
+                	fp.appendFilters(nsIFilePicker.filterAll | nsIFilePicker.filterText);
+		}	
+		catch(err){
+			alert(err);
+			return;
+		}
                 var rv = fp.show();
                 if (rv == nsIFilePicker.returnOK || rv == nsIFilePicker.returnReplace) {
                 	var file = fp.file;
@@ -15,7 +21,7 @@ var opwindowCommon={
          },
 	 myComponentTestGo:function()
 	 {
-		alert("Start of MyComponentTestGo()");
+		//alert("Start of MyComponentTestGo()");
         	try {
                		netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
                 	const cid = "@mydomain.com/XPCOMSample/MyComponent;1";
@@ -29,7 +35,31 @@ var opwindowCommon={
         	}
         	var res = obj.Add(3, 4);
         	alert('Performing 3+4. Returned ' + res + '.');
+	},
+	uploadFile:function()
+	{
+		try{
+			var filePath = document.getElementById("file-path").value;
+			var file = Components.classes['@mozilla.org/file/local;1'].createInstance(Components.interfaces.nsILocalFile);
+			file.initWithPath(filePath);
+			var fstream = Components.classes['@mozilla.org/network/file-input-stream;1'].createInstance(Components.interfaces.nsIFileInputStream);
+			fstream.init(file, 1, 1, Components.interfaces.nsIFileInputStream.CLOSE_ON_EOF);
+			var bstream = Components.classes['@mozilla.org/network/buffered-input-stream;1'].createInstance(Components.interfaces.nsIBufferedInputStream);
+			bstream.init(fstream, 4096);
+			xhr = new XMLHttpRequest();
+		}
+		catch(err){
+			alert(err);
+			return;
+		}
+	        xhr.onreadystatechange = function() { if(this.readyState == 4) {alert(this.responseText)}}
+		xhr.open("POST", "http://localhost/upload.php", true);
+		xhr.setRequestHeader('Content-Type', 'application/octet-stream');
+		xhr.send(bstream);
+		
+
 	}
+	
 
 }
 
