85d7209c3680629c85035beddd4b9e2db698b257
nithin <nithin@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2008-08-03 13:12:26 +0000

    update of gui and updation progress. still not functional

--- oggPusher/firefox-extension/chrome/content/op_Window.js
+++ oggPusher/firefox-extension/chrome/content/op_Window.js
@@ -1,25 +1,52 @@
-var opwindowCommon={leafName:null, timer:null,
+var opwindowCommon={leafName:null, timer:null,isOgg:null,
 	openFileWindowDialog:function()
     {
 		//alert("Inside openFileWindowDialog");
 		try{
-                	const nsIFilePicker = Components.interfaces.nsIFilePicker;
+           	const nsIFilePicker = Components.interfaces.nsIFilePicker;
 			var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
-        	        fp.init(window, "Choose a File", nsIFilePicker.modeOpen);
-                	fp.appendFilters(nsIFilePicker.filterAll | nsIFilePicker.filterText);
+        	fp.init(window, "Choose a File", nsIFilePicker.modeOpen);
+           	fp.appendFilters(nsIFilePicker.filterAll | nsIFilePicker.filterText);
 		}	
 		catch(err){
 			alert(err);
 			return;
 		}
-                var rv = fp.show();
-                if (rv == nsIFilePicker.returnOK || rv == nsIFilePicker.returnReplace) {
-                	var file = fp.file;
-         		var path = fp.file.path;
-			//alert(fp.file.leafName);
-			this.leafName = fp.file.leafName;
+        
+        var rv = fp.show();
+        if (rv == nsIFilePicker.returnOK || rv == nsIFilePicker.returnReplace) {
+            var file = fp.file;
+         	var path = fp.file.path;
+            //alert(fp.file.leafName);
+            this.leafName = fp.file.leafName;
 			parent.document.getElementById("file-path").value = path;
-                }
+            this.appendActionButton();    
+            
+        }
+        
+    },
+    isOggFormat:function(){
+        
+    },
+    appendActionButton:function(){
+        
+        var hboxParent = document.getElementById("op-input");
+        
+        var actionButton = document.getElementById("action-button");
+        if(actionButton == null){
+            actionButton = document.createElement("button");
+            actionButton.setAttribute("id","action-button");
+            actionButton.setAttribute("oncommand","actionButtonHandler();");
+            actionButton.setAttribute("image","chrome://oggPusher/content/images/xiph.ico");
+            actionButton.setAttribute("orient","vertical");
+            actionButton.setAttribute("label","Upload Audio/Video");
+            hboxParent.appendChild(actionButton);
+        }
+    },
+    actionButtonHandler:function(){
+        
+        
+        
     },
 	chooseDestination:function()
 	{
@@ -87,35 +114,36 @@ var opwindowCommon={leafName:null, timer:null,
 	myComponentTestGo:function()
 	{
 		//alert("Start of MyComponentTestGo()");
-        	try {
-               		netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
-                	const cid = "@mydomain.com/XPCOMSample/MyComponent;1";
-                	obj = Components.classes[cid].createInstance();
-                	obj = obj.QueryInterface(Components.interfaces.IMyComponent);
-        	} 
+        try {
+        	netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+           	const cid = "@mydomain.com/XPCOMSample/MyComponent;1";
+           	obj = Components.classes[cid].createInstance();
+           	obj = obj.QueryInterface(Components.interfaces.IMyComponent);
+        } 
 		catch (err) 
 		{
-	               	alert(err);
-        	        return;
-        	}
-        	var res = obj.Add(3, 4);
-        	alert('Performing 3+4. Returned ' + res + '.');
+	      	alert(err);
+            return;
+        }
+        var res = obj.Add(3, 4);
+        alert('Performing 3+4. Returned ' + res + '.');
 	},
 	uploadFile:function()
 	{
-			var textboxObj = document.getElementById("file-path");
-			//alert(textboxObj);
-			var filePath = textboxObj.value;
-			//alert(filePath);
-			var file = Components.classes['@mozilla.org/file/local;1'].createInstance(Components.interfaces.nsILocalFile);
-			file.initWithPath(filePath);
-			var fstream = Components.classes['@mozilla.org/network/file-input-stream;1'].createInstance(Components.interfaces.nsIFileInputStream);
-			fstream.init(file, 1, 1, Components.interfaces.nsIFileInputStream.CLOSE_ON_EOF);
-			var bstream = Components.classes['@mozilla.org/network/buffered-input-stream;1'].createInstance(Components.interfaces.nsIBufferedInputStream);
-			bstream.init(fstream, 4096);
-			var binary = Components.classes["@mozilla.org/binaryinputstream;1"].createInstance(Components.interfaces.nsIBinaryInputStream);
-			binary.setInputStream(fstream);
-			xhr = new XMLHttpRequest();
+		var textboxObj = document.getElementById("file-path");
+		//alert(textboxObj);
+		var filePath = textboxObj.value;
+		//alert(filePath);
+        
+		var file = Components.classes['@mozilla.org/file/local;1'].createInstance(Components.interfaces.nsILocalFile);
+		file.initWithPath(filePath);
+		var fstream = Components.classes['@mozilla.org/network/file-input-stream;1'].createInstance(Components.interfaces.nsIFileInputStream);
+		fstream.init(file, 1, 1, Components.interfaces.nsIFileInputStream.CLOSE_ON_EOF);
+		var bstream = Components.classes['@mozilla.org/network/buffered-input-stream;1'].createInstance(Components.interfaces.nsIBufferedInputStream);
+		bstream.init(fstream, 4096);
+		var binary = Components.classes["@mozilla.org/binaryinputstream;1"].createInstance(Components.interfaces.nsIBinaryInputStream);
+		binary.setInputStream(fstream);
+		xhr = new XMLHttpRequest();
 		
 		
 
@@ -132,12 +160,11 @@ var opwindowCommon={leafName:null, timer:null,
 		
 		//alert(requestbody);
 
-	        xhr.onreadystatechange = function() { if(this.readyState == 4) {}}
+	    xhr.onreadystatechange = function() { if(this.readyState == 4) {alert("Uploading Done Successfully.");}}
 		xhr.open("POST", "http://www.it.iitb.ac.in/~nithind/post.php", true);
 		xhr.setRequestHeader("Content-Type", "multipart/form-data; boundary=\""+ boundaryString + "\"");
 		xhr.setRequestHeader("Connection", "close");
 		xhr.setRequestHeader("Content-length", requestbody.length);
-		//alert(requestbody.length);
 		xhr.send(requestbody);
 		
 
