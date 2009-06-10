fc8eef2df33eb5e4d0b069fe90c577d56ea69517
xiphmont <xiphmont@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2009-06-10 11:24:30 +0000

    restarting; branch from 1.2.2rc1

--- cdparanoia/report.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "interface/cdda_interface.h"
-
-int quiet=0;
-int verbose=CDDA_MESSAGE_FORGETIT;
-FILE *reportfile=NULL;
--- ffmpeg2theora/src/iso639.h
+++
@@ -1,7 +0,0 @@
-#ifndef _F2T_ISO639_H_
-#define _F2T_ISO639_H_
-
-extern const char *find_iso639_1(const char *iso639_2);
-
-#endif
-
