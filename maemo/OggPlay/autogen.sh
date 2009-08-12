9a8a5d4323a56ad7413fe288edd6ab4cf8973ec3
xiphmont <xiphmont@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2009-08-12 03:10:31 +0000

    Monty branch for non-mainline experimental work / work intended to land post 1.1

--- maemo/OggPlay/autogen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -x
-aclocal-1.7
-autoconf
-libtoolize
-automake-1.7 --add-missing --foreign
--- win32-tools/oggdrop/encthread.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ENCTHREAD_H__
-#define __ENCTHREAD_H__
-
-void encthread_init(void);
-void encthread_addfile(char *file);
-void encthread_setquality(int quality);
-void encthread_setbitrate(int bitrate);
-
-#endif
