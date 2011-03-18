9def46a82bc3203573c9a186a85454431fb8aa30
xiphmont <xiphmont@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2011-03-18 06:06:42 +0000

    continue moving around chirp estimation code

---
+++ ghost/monty/chirp/chirp.h
@@ -0,0 +1,29 @@
+/********************************************************************
+ *                                                                  *
+ * THIS FILE IS PART OF THE OggGhost SOFTWARE CODEC SOURCE CODE.    *
+ * USE, DISTRIBUTION AND REPRODUCTION OF THIS LIBRARY SOURCE IS     *
+ * GOVERNED BY A BSD-STYLE SOURCE LICENSE INCLUDED WITH THIS SOURCE *
+ * IN 'COPYING'. PLEASE READ THESE TERMS BEFORE DISTRIBUTING.       *
+ *                                                                  *
+ * THE OggGhost SOURCE CODE IS (C) COPYRIGHT 2007-2011              *
+ * by the Xiph.Org Foundation http://www.xiph.org/                  *
+ *                                                                  *
+ ********************************************************************
+
+ function: research-grade chirp extraction code
+ last mod: $Id$
+
+ ********************************************************************/
+
+typedef struct {
+  float A;  /* center amplitude (linear) */
+  float W;  /* frequency (radians per sample, not cycles per block) */
+  float P;  /* phase (radians) */
+  float dA; /* amplitude modulation (linear change per sample) */
+  float dW; /* frequency modulation (radians per sample^2) */
+} chirp;
+
+extern int estimate_chirps(float *x, float *y, float *window, int len,
+                           chirp *c, int n, int iter_limit, float fit_limit);
+extern void advance_chirps(chirp *c, int n, int len);
+
