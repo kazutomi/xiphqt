9c0470ccac5955800e2781991ecd798c5b6656c1
xiphmont <xiphmont@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2012-05-21 05:25:19 +0000

    Update copyrights

--- spectrum/spec_panel.c
+++ spectrum/spec_panel.c
@@ -1,19 +1,19 @@
 /*
  *
  *  gtk2 spectrum analyzer
- *    
- *      Copyright (C) 2004 Monty
+ *
+ *      Copyright \(C\) 2004-2012 Monty
  *
  *  This analyzer is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2, or (at your option)
  *  any later version.
- *   
+ *
  *  The analyzer is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *   
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with Postfish; see the file COPYING.  If not, write to the
  *  Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -735,7 +735,7 @@ void panel_create(struct panel *panel){
   /* mode */
   {
     GtkWidget *menu=gtk_combo_box_new_text();
-    char *entries[]={"realtime","maximum","accumulate","average"};
+    char *entries[]={"current frame","maximum","accumulate","average"};
     for(i=0;i<4;i++)
       gtk_combo_box_append_text (GTK_COMBO_BOX (menu), entries[i]);
     gtk_combo_box_set_active(GTK_COMBO_BOX(menu),plot_mode);
