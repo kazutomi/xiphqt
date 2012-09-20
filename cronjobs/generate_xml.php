16ec6fd7d7b8bca7e1194c4007a3bc813195a06c
dm8tbr <dm8tbr@0101bb08-14d6-0310-b084-bc0e0c8e3800>
2012-09-20 07:37:11 +0000

    dir.xiph.org - remove limitation in yp.xml output length.

--- cronjobs/generate_xml.php
+++ cronjobs/generate_xml.php
@@ -3,7 +3,7 @@
 // Inclusions
 include_once(dirname(__FILE__).'/../inc/prepend.php');
 
-ini_set('memory_limit', '32M');
+ini_set('memory_limit', '64M');
 
 define('XML_OUTPUT', dirname(__FILE__).'/../yp.xml');
 
@@ -11,7 +11,8 @@ define('XML_OUTPUT', dirname(__FILE__).'/../yp.xml');
 $db = DirXiphOrgDBC::getInstance();
 
 // Get data
-$query = "SELECT m.`stream_name`, s.`listen_url`, m.`media_type_id`, m.`bitrate`, m.`channels`, m.`samplerate`, GROUP_CONCAT(t.`tag_name` SEPARATOR ' ') AS `genre`, m.`current_song` FROM `mountpoint` AS m INNER JOIN `server` AS s ON m.`id` = s.`mountpoint_id` INNER JOIN `mountpoints_tags` AS mt ON mt.`mountpoint_id` = m.`id` INNER JOIN `tag` AS t ON mt.`tag_id` = t.`id` GROUP BY s.`id` ORDER BY m.`listeners` DESC LIMIT 1000;";
+//$query = "SELECT m.`stream_name`, s.`listen_url`, m.`media_type_id`, m.`bitrate`, m.`channels`, m.`samplerate`, GROUP_CONCAT(t.`tag_name` SEPARATOR ' ') AS `genre`, m.`current_song` FROM `mountpoint` AS m INNER JOIN `server` AS s ON m.`id` = s.`mountpoint_id` INNER JOIN `mountpoints_tags` AS mt ON mt.`mountpoint_id` = m.`id` INNER JOIN `tag` AS t ON mt.`tag_id` = t.`id` GROUP BY s.`id` ORDER BY m.`listeners` DESC LIMIT 1000;";
+$query = "SELECT m.`stream_name`, s.`listen_url`, m.`media_type_id`, m.`bitrate`, m.`channels`, m.`samplerate`, GROUP_CONCAT(t.`tag_name` SEPARATOR ' ') AS `genre`, m.`current_song` FROM `mountpoint` AS m INNER JOIN `server` AS s ON m.`id` = s.`mountpoint_id` INNER JOIN `mountpoints_tags` AS mt ON mt.`mountpoint_id` = m.`id` INNER JOIN `tag` AS t ON mt.`tag_id` = t.`id` GROUP BY s.`id` ORDER BY m.`listeners` DESC;";
 $res = $db->selectQuery($query)->array_data;
 shuffle($res);
 $tpl->assign_by_ref('streams', $res);
